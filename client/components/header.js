43cbf85d350b2bb898a33113d9cc7beb6bda0f18
sj <jainsanmati846@gmail.com>
2019-09-29 02:30:26 +0530

    Implement link redirect

--- client/components/header.js
+++ client/components/header.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import LinkCreate from './link_create';
+import { Links } from '../../imports/collections/links';
+import LinkList from './../components/link_list';
 
 const Header = () => {
   return (
-    <div className='nav navbar-default'>
-      <div className='navbar-header'>
-        <a className='navbar-brand'>Shorten My Link</a>
+    <div className='container'>
+      <div className='nav navbar-default'>
+        <div className='navbar-header'>
+          <a className='navbar-brand'>Shorten My Link</a>
+        </div>
       </div>
+      <LinkCreate />
+      <LinkList />
     </div>
   );
 };
